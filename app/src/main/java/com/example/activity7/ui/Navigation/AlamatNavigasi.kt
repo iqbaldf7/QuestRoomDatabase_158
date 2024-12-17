16f61ba7e1d221c872834a86c5cab8cdeec6ddbf
iqbaldf7 <Iqbaldathonah10@gmail.com>
2024-12-17 22:48:24 +0700

    melakukan perbaikan eror pada AlamatNavigasi

--- app/src/main/java/com/example/activity7/ui/Navigation/AlamatNavigasi.kt
+++ app/src/main/java/com/example/activity7/ui/Navigation/AlamatNavigasi.kt
@@ -1,21 +1,21 @@
 package com.example.activity7.ui.Navigation
 
-interface Alamatnavigasi {
-    val route : String
+interface AlamatNavigasi {
+    val route: String
 }
 
-object DestinasiHome : Alamatnavigasi {
+object DestinasiHome: AlamatNavigasi{
     override val route = "home"
 }
 
-object DestinasiDetail : Alamatnavigasi {
-    override  val route = "detail"
+object DestinasiDetail : AlamatNavigasi{
+    override val route = "detail"
     const val NIM = "nim"
     val routesWithArg = "$route/{$NIM}"
 }
 
-object DestinasiUpdate : Alamatnavigasi {
+object DestinasiUpdate : AlamatNavigasi {
     override val route = "update"
     const val NIM = "nim"
-    val routeWithArg = "$route/{$NIM}"
+    val  routesWithArg = "$route/{$NIM}"
 }
