2929ed45fbf40bb291c7019b2a1f0d6f5b30f349
iqbaldf7 <Iqbaldathonah10@gmail.com>
2024-12-17 07:33:28 +0700

    untuk mengelola data mahasiswa DAO

--- app/src/main/java/com/example/activity7/Repository/RepositoryMhs.kt
+++ app/src/main/java/com/example/activity7/Repository/RepositoryMhs.kt
@@ -1,7 +1,15 @@
 package com.example.activity7.Repository
 
 import com.example.activity7.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 interface RepositoryMhs {
     suspend fun insertMhs(mahasiswa: Mahasiswa)
+    fun getAllMhs(): Flow<List<Mahasiswa>>
+
+    fun getMhs(nim: String): Flow<Mahasiswa>
+
+    suspend fun deleteMhs(mahasiswa: Mahasiswa)
+
+    suspend fun updateMhs(mahasiswa: Mahasiswa)
 }
