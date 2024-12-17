34925fcbcded0481b2035be9d7912b6c0da07e46
iqbaldf7 <Iqbaldathonah10@gmail.com>
2024-12-17 15:55:47 +0700

    Memperbaiki bagian AlamatNavigasi

--- app/src/main/java/com/example/activity7/ui/Navigation/AlamatNavigasi.kt
+++ app/src/main/java/com/example/activity7/ui/Navigation/AlamatNavigasi.kt
@@ -1,22 +1,21 @@
 package com.example.activity7.ui.Navigation
 
-interface AlamatNavigasi {
-    val route: String
+interface Alamatnavigasi {
+    val route : String
 }
 
-object DestinasiHome: AlamatNavigasi{
+object DestinasiHome : Alamatnavigasi {
     override val route = "home"
 }
 
-object DestinasiDetail : AlamatNavigasi{
-    override val route = "detail"
+object DestinasiDetail : Alamatnavigasi {
+    override  val route = "detail"
     const val NIM = "nim"
     val routesWithArg = "$route/{$NIM}"
 }
 
-object DestinasiUpdate : AlamatNavigasi {
+object DestinasiUpdate : Alamatnavigasi {
     override val route = "update"
     const val NIM = "nim"
-    val  routesWithArg = "$route/{$NIM}"
+    val routeWithArg = "$route/{$NIM}"
 }
-
