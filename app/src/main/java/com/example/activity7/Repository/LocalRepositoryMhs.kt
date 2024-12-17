0410f2c1beccaa6062d0f3635b614357ae274ae5
iqbaldf7 <Iqbaldathonah10@gmail.com>
2024-12-17 07:37:09 +0700

    menyimpan data mahasiswa ke dalam DAO

--- app/src/main/java/com/example/activity7/Repository/LocalRepositoryMhs.kt
+++ app/src/main/java/com/example/activity7/Repository/LocalRepositoryMhs.kt
@@ -2,12 +2,34 @@ package com.example.activity7.Repository
 
 import com.example.activity7.data.dao.MahasiswaDao
 import com.example.activity7.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
+
 
 class LocalRepositoryMhs(
     private val mahasiswaDao: MahasiswaDao
-): RepositoryMhs {
+) : RepositoryMhs {
+
+
     override suspend fun insertMhs(mahasiswa: Mahasiswa) {
-       mahasiswaDao.insertMahasiswa(mahasiswa)
+        mahasiswaDao.insertMahasiswa(mahasiswa)
     }
 
+
+    override fun getAllMhs(): Flow<List<Mahasiswa>> {
+        return mahasiswaDao.getAllMahasiswa()
+    }
+
+    override fun getMhs(nim: String): Flow<Mahasiswa> {
+        return mahasiswaDao.getMahasiswa(nim)
+    }
+
+    override suspend fun deleteMhs(mahasiswa: Mahasiswa) {
+        mahasiswaDao.deletedMahasiswa(mahasiswa)
+    }
+
+    override suspend fun updateMhs(mahasiswa: Mahasiswa) {
+        mahasiswaDao.updateMahasiswa(mahasiswa)
+    }
 }
+
+
